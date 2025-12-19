9d585cff568e1f1251a7ea8b30455c475825b3f7
Inozowa <shane.m.hatch@gmail.com>
2025-12-19 05:39:19 -0800

    Update ActionDeployObject.c

--- WorkbenchV3/Scripts/4_World/Classes/Actions/Modded/ActionDeployObject.c
+++ WorkbenchV3/Scripts/4_World/Classes/Actions/Modded/ActionDeployObject.c
@@ -1,7 +1,7 @@
 modded class ActionDeployObject : ActionDeployBase {
 	override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item) {	
-		if (!player||!item) return false;
-		if (item.IsInherited(BaseBuildingBase) && !item.CanFindFBWorkBench()) return false;
-		return super.ActionCondition(player, target, item);
+		if (!super.ActionCondition(player, target, item)) return false;
+		if (item.IsInherited(DeployableContainer_Base)) return true;
+		return item.CanFindFBWorkBench();
 	}
 }
