73b9e83807c7ed6a2e301f7a5f56dde61c33c751
Inozowa <shane.m.hatch@gmail.com>
2025-12-19 05:41:33 -0800

    Create ActionPlaceObject.c

---
+++ WorkbenchV3/Scripts/4_World/Classes/Actions/Modded/ActionPlaceObject.c
@@ -0,0 +1,7 @@
+modded class ActionPlaceObject : ActionDeployObject {
+	override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item) {	
+		if (!super.ActionCondition(player, target, item)) return false;
+		if (item.IsInherited(DeployableContainer_Base)) return true;
+		return item.CanFindFBWorkBench();
+	}
+}
