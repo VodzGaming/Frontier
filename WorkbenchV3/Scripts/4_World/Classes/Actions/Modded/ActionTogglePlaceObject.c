64bbf58d2bc718dd0c0789810dbef5bdb8105c8f
Inozowa <shane.m.hatch@gmail.com>
2025-12-19 05:39:02 -0800

    Update ActionTogglePlaceObject.c

--- WorkbenchV3/Scripts/4_World/Classes/Actions/Modded/ActionTogglePlaceObject.c
+++ WorkbenchV3/Scripts/4_World/Classes/Actions/Modded/ActionTogglePlaceObject.c
@@ -1,4 +1,7 @@
-class ActionTogglePlaceObject : ActionSingleUseBase
-{
-
+modded class ActionTogglePlaceObject : ActionSingleUseBase {
+	override bool ActionCondition(PlayerBase player, ActionTarget target, ItemBase item) {	
+		if (!super.ActionCondition(player, target, item)) return false;
+		if (item.IsInherited(DeployableContainer_Base)) return true;
+		return item.CanFindFBWorkBench();
+	}
 }
